5b97483f8249f090338f1ce5517117c3d74e7993
Bunny67 <zolov6767@gmail.com>
2020-03-27 17:12:22 +0300

    fix display wotlk revision

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -42,6 +42,10 @@ end
 
 local function showRealDate(curseDate)
 	curseDate = tostring(curseDate)
+	if curseDate and #curseDate < 10 then
+		return curseDate
+	end
+
 	local year, month, day, hour, minute, second = curseDate:sub(1, 4), curseDate:sub(5, 6), curseDate:sub(7, 8), curseDate:sub(9, 10), curseDate:sub(11, 12), curseDate:sub(13, 14)
 	if year and month and day and hour and minute and second then
 		return year.."/"..month.."/"..day.." "..hour..":"..minute..":"..second
