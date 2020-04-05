780ed4fc84b48c70e71b0504fc6fdef2c3b0344b
Bunny67 <zolov6767@gmail.com>
2020-04-05 13:54:44 +0300

    remove unused localizations

---
+++ DBM-Magtheridon/localization.en.lua
@@ -0,0 +1,30 @@
+﻿if GetLocale() ~= "ruRU" then return end
+
+local L
+
+--Magtheridon
+L = DBM:GetModLocalization("Magtheridon")
+
+L:SetGeneralLocalization{
+    name = "Магтеридон "
+}
+
+L:SetTimerLocalization{
+    Pull = "Активация босса"
+}
+
+L:SetWarningLocalization{
+    WarnPhase2soon = "Скоро фаза 2",
+    WarnHandOfMagt = "Печать Магтеридона"
+}
+
+L:SetOptionLocalization{
+    WarnPhase2soon = "Анонсировать переход на вторую фазу",
+    WarnHandOfMagt = "Спец предупреждение для целей Печати",
+    Pull = "Отсчет времени до активации босса"
+}
+
+L:SetMiscLocalization{
+    YellPhase2 = "Меня так просто не возьмешь! Пусть стены темницы содрогнутся... и падут!"
+}
+
