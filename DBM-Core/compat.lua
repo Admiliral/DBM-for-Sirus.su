94fab33f5c78f356e94c1333311d9f1dd75010be
Bunny67 <zolov6767@gmail.com>
2020-04-07 17:12:32 +0300

    Update compat.lua

--- DBM-Core/compat.lua
+++ DBM-Core/compat.lua
@@ -1,6 +1,7 @@
 if C_Timer and C_Timer._version == 2 then return end
 
 local setmetatable = setmetatable
+local type = type
 local tinsert = table.insert
 local tremove = table.remove
 
@@ -15,26 +16,30 @@ local TickerMetatable = {
 
 local waitTable = {}
 local waitFrame = TimerFrame or CreateFrame("Frame", "TimerFrame", UIParent)
-waitFrame:SetScript("OnUpdate", function (self, elapsed)
+waitFrame:SetScript("OnUpdate", function(self, elapsed)
 	local total = #waitTable
 	local i = 1
 
 	while i <= total do
-		local data = waitTable[i]
+		local ticker = waitTable[i]
 
-		if data[2]._cancelled then
+		if ticker._cancelled then
 			tremove(waitTable, i)
 			total = total - 1
-		elseif data[1] > elapsed then
-			data[1] = data[1] - elapsed
+		elseif ticker._delay > elapsed then
+			ticker._delay = ticker._delay - elapsed
 			i = i + 1
 		else
-			data[2]._callback()
+			ticker._callback(ticker)
 
-			if data[2]._remainingIterations == -1 then
-				data[1] = data[2]._duration
+			if ticker._remainingIterations == -1 then
+				ticker._delay = ticker._duration
 				i = i + 1
-			elseif data[2]._remainingIterations == 0 then
+			elseif ticker._remainingIterations > 0 then
+				ticker._remainingIterations = ticker._remainingIterations - 1
+				ticker._delay = ticker._duration
+				i = i + 1
+			elseif ticker._remainingIterations == 0 then
 				tremove(waitTable, i)
 				total = total - 1
 			end
@@ -46,8 +51,12 @@ waitFrame:SetScript("OnUpdate", function (self, elapsed)
 	end
 end)
 
-local function AddDelayedCall(delay, func)
-	tinsert(waitTable, {delay, func})
+local function AddDelayedCall(ticker, oldTicker)
+	if oldTicker and type(oldTicker) == "table" then
+		ticker = oldTicker
+	end
+
+	tinsert(waitTable, ticker)
 	waitFrame:Show()
 end
 
@@ -57,15 +66,10 @@ function C_Timer:NewTicker(duration, callback, iterations)
 	local ticker = setmetatable({}, TickerMetatable)
 	ticker._remainingIterations = iterations or -1
 	ticker._duration = duration
-	ticker._callback = function()
-		callback(ticker)
+	ticker._delay = duration
+	ticker._callback = callback
 
-		if ticker._remainingIterations > 0 then
-			ticker._remainingIterations = ticker._remainingIterations - 1
-		end
-	end
-
-	AddDelayedCall(ticker._duration, ticker)
+	AddDelayedCall(ticker)
 
 	return ticker
 end
