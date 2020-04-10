3d8af268aa3b670a5638dc04d4254c472d8a54f4
Admiliral <62554449+Admiliral@users.noreply.github.com>
2020-04-11 01:30:54 +0500

    fix DBM:GetSpellInfo

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -73,9 +73,9 @@ local function showRealDate(curseDate)
 end
 
 DBM = {
-	Revision = parseCurseDate("202004101174223"),
+	Revision = parseCurseDate("20200410233000"),
 	DisplayVersion = "5.40", -- the string that is shown as version
-	ReleaseRevision = releaseDate(2020, 4, 5) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	ReleaseRevision = releaseDate(2020, 4, 10) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
