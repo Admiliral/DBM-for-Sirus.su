edc5aa9dd8bf4a4eadd21298a6203acd9d5c6614
Waini4 <pirat.33370@gmail.com>
2021-08-23 21:33:15 +0300

    Revision core

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -69,9 +69,9 @@ local function showRealDate(curseDate)
 end
 
 DBM = {
-	Revision = parseCurseDate("20210820232900"),
+	Revision = parseCurseDate("20210823213200"),
 	DisplayVersion = "5.51", -- the string that is shown as version
-	ReleaseRevision = releaseDate(2021, 08, 20) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	ReleaseRevision = releaseDate(2021, 08, 23) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
