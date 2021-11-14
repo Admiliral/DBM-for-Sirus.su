b11747ade1d3ddf5f4104c5854c79df989af64ed
Waini4 <pirat.33370@gmail.com>
2021-11-14 22:46:29 +0200

    Поправил начальные тайминги на 3 боссе + фикс для танков на 1-2 боссе

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -70,9 +70,9 @@ end
 
 
 DBM = {
-	Revision = parseCurseDate("20211113184900"),
+	Revision = parseCurseDate("20211114224900"),
 	DisplayVersion = "5.52", -- the string that is shown as version
-	ReleaseRevision = releaseDate(2021, 11, 13) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	ReleaseRevision = releaseDate(2021, 11, 14) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
