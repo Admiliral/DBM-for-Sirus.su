ec032feaf4476207256c45daf458868aac8d5113
Waini4 <pirat.33370@gmail.com>
2021-12-16 00:55:19 +0200

    f

--- DBM-Tol'Garod/Norigorn.lua
+++ DBM-Tol'Garod/Norigorn.lua
@@ -3,7 +3,6 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("20210501000000")
 mod:SetCreatureID(70010)
-mod:EnableWBEngageSync()--Enable syncing engage in outdoors
 
 mod:RegisterCombat("combat", 70010)
 
