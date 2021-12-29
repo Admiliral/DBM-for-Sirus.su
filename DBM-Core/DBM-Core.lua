ed0554705def7d1da15ad291a4406b07fbeeeb73
Waini4 <pirat.33370@gmail.com>
2021-12-29 22:32:40 +0200

    Update DBM-Core.lua

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -70,9 +70,9 @@ end
 
 
 DBM = {
-	Revision = parseCurseDate("20211215170100"),
+	Revision = parseCurseDate("20211229223200"),
 	DisplayVersion = "5.54 by Waini", -- the string that is shown as version
-	ReleaseRevision = releaseDate(2021, 12, 15) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	ReleaseRevision = releaseDate(2021, 12, 29) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
