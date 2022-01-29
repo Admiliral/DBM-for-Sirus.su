92d6310ef445e5030a5890ac0942beea86aa30d2
fxpw <fxpw@outlook.com>
2022-01-29 17:05:17 +0300

    add gruul

--- DBM-Gruul/Gruul.lua
+++ DBM-Gruul/Gruul.lua
@@ -17,7 +17,9 @@ local timerEchoCD                   = mod:NewCDTimer(18, 36297)
 local timerRockCD                   = mod:NewCDTimer(30, 36240)
 local specWarnRock                  = mod:NewSpecialWarningMove(36240)
 
-local timerHandCD                   = mod:NewCDTimer(18, 305188)
+local timerHandCD                   = mod:NewCDTimer(29, 305188)
+local timerHateStrike		    = mod:NewCDTimer(6, 305197)
+local timerStunningBlow		    = mod:NewCDTimer(20, 305183)
 local timerHandStrike               = mod:NewTimer(7,"Strike", 305188)
 local timerFurnaceActive            = mod:NewTimer(8,"TimerFurnaceActive", 305201)
 local timerFurnaceInactive          = mod:NewTimer(43,"TimerFurnaceInactive", 305201)
@@ -28,10 +30,12 @@ local rockCounter = 1
 function mod:OnCombatStart(delay)
 	DBM:FireCustomEvent("DBM_EncounterStart", 19044, "Gruul the Dragonkiller")
 	if mod:IsDifficulty("heroic25") or mod:IsDifficulty("heroic10") then
-		timerHandCD:Start(15)
+		timerHandCD:Start(24)
+		timerHateStrike:Start(20)
+		timerStunningBlow:Start(15)
 	elseif mod:IsDifficulty("normal25") or mod:IsDifficulty("normal10") then
 		timerEarthStrikeCD:Start(35)
-		timerRockCD:Start(26)
+		timerRockCD:Start(28)
 		rockCounter = 1
 	end
 end
@@ -46,7 +50,11 @@ function mod:SPELL_CAST_START(args)
 		 timerEchoCD:Start(25)
 		 timerRockCD:Start(30 - rockCounter*2 + 4)
 		 if rockCounter <= 11 then rockCounter = rockCounter + 1 end
-	end
+		elseif args:IsSpellID(305197) then
+			timerHateStrike:Start()
+		elseif args:IsSpellID(305183) then
+			timerStunningBlow:Start()
+		end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
