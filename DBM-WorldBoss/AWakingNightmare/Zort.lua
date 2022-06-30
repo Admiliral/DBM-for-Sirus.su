28bb6e432401b2dd9db47df6b5957923c8e1013b
Waini4 <pirat.33370@gmail.com>
2022-06-30 18:24:31 +0300

    Мини зорт

--- DBM-WorldBoss/AWakingNightmare/Zort.lua
+++ DBM-WorldBoss/AWakingNightmare/Zort.lua
@@ -1,5 +1,5 @@
-local mod	= DBM:NewMod("Zort", "DBM-WorldBoss", 2)
-local L		= mod:GetLocalizedStrings()
+local mod = DBM:NewMod("Zort", "DBM-WorldBoss", 2)
+local L   = mod:GetLocalizedStrings()
 
 mod:SetRevision(("20220616165100"):sub(12, -3))
 mod:SetCreatureID(50702)
@@ -11,61 +11,62 @@ mod:RegisterEvents(
 	"SPELL_CAST_START",
 	"SPELL_CAST_SUCCESS",
 	"SPELL_AURA_APPLIED",
-    "SPELL_AURA_APPLIED_DOSE",
+	"SPELL_AURA_APPLIED_DOSE",
 	"UNIT_TARGET",
-    "SPELL_DAMAGE",
-    "SPELL_PERIODIC_DAMAGE",
+	"SPELL_DAMAGE",
+	"SPELL_PERIODIC_DAMAGE",
 	"SPELL_AURA_REMOVED",
 	"SPELL_INTERRUPT",
 	"SPELL_CAST_FAILED",
-    "UNIT_HEALTH",
+	"UNIT_HEALTH",
 	"UNIT_DIED",
 	"SWING_DAMAGE"
 )
 
 
-local warnPhase2Soon   					= mod:NewPrePhaseAnnounce(2)
-local warnPhase2     					= mod:NewPhaseAnnounce(2)
-local warnPhase3Soon   					= mod:NewPrePhaseAnnounce(3)
-local warnPhase3     					= mod:NewPhaseAnnounce(3)
-local warnPech							= mod:NewTargetAnnounce(307814, 2)
-local warnFlame							= mod:NewTargetAnnounce(307839, 2)
-local warnSveaz							= mod:NewTargetAnnounce(308517, 3)
-local warnkik							= mod:NewCastAnnounce(307829, 2)
-local warnPriziv						= mod:NewCastAnnounce(307852, 3)
-local warnShkval						= mod:NewCastAnnounce(307821, 3)
-local warnTraitor						= mod:NewCountAnnounce(307814, 2, nil, false)
-local warnInternalbleeding	    		= mod:NewStackAnnounce(307833, 2, nil, "Tank|Healer")
-local warnInternalbgPre	    			= mod:NewPreWarnAnnounce(307833, 5, nil, nil, "Tank|Healer")
-local specWarnBreathNightmare	    	= mod:NewSpecialWarningDispel(308512, "RemoveDisease", nil, nil, 1, 6)
+local warnPhase2Soon          = mod:NewPrePhaseAnnounce(2)
+local warnPhase2              = mod:NewPhaseAnnounce(2)
+local warnPhase3Soon          = mod:NewPrePhaseAnnounce(3)
+local warnPhase3              = mod:NewPhaseAnnounce(3)
+local warnPech                = mod:NewTargetAnnounce(307814, 2)
+local warnFlame               = mod:NewTargetAnnounce(307839, 2)
+local warnSveaz               = mod:NewTargetAnnounce(308517, 3)
+local warnkik                 = mod:NewCastAnnounce(307829, 2)
+local warnPriziv              = mod:NewCastAnnounce(307852, 3)
+local warnShkval              = mod:NewCastAnnounce(307821, 3)
+local warnTraitor             = mod:NewCountAnnounce(307814, 2, nil, false)
+local warnInternalbleeding    = mod:NewStackAnnounce(307833, 2, nil, "Tank|Healer")
+local warnInternalbgPre       = mod:NewPreWarnAnnounce(307833, 5, nil, nil, "Tank|Healer")
+local specWarnBreathNightmare = mod:NewSpecialWarningDispel(308512, "RemoveDisease", nil, nil, 1, 6)
 
-local specWarnRazrsveaz 				= mod:NewSpecialWarning("KnopSv", 3)
-local specCowardice	           			= mod:NewSpecialWarning("|cff71d5ff|Hspell:307834|hПечать: Трусость|h|r Бей босcа - Держи радиус 6 метров!")
-local specWarnshkval					= mod:NewSpecialWarningGTFO(307821, nil, nil, nil, 1, 2)
-local specWarnTraitor					= mod:NewSpecialWarningStack(307814, nil, 2, nil, nil, 3, 6)
-local specWarnReturnInterrupt			= mod:NewSpecialWarningInterrupt(307829, "HasInterrupt", nil, 2, 1, 2)
-local specWarnPechati					= mod:NewSpecialWarningCast(307814, nil, nil, nil, 1, 2) --предатель
-local specWarnFlame						= mod:NewSpecialWarningMoveAway(307839, nil, nil, nil, 3, 2)
-local specWarnSveaz						= mod:NewSpecialWarningYou(308516, nil, nil, nil, 3, 2)
-local yellFlame							= mod:NewYell(307839, nil, nil, nil, "YELL") --Огонь
-local yellFlameFade						= mod:NewShortFadesYell(307839, nil, nil, nil, "YELL")
-local yellCastsvFade					= mod:NewShortFadesYell(308520)
+local specWarnRazrsveaz       = mod:NewSpecialWarning("KnopSv", 3)
+local specCowardice           = mod:NewSpecialWarning("|cff71d5ff|Hspell:307834|hПечать: Трусость|h|r Бей босcа - Держи радиус 6 метров!")
+local specWarnshkval          = mod:NewSpecialWarningGTFO(307821, nil, nil, nil, 1, 2)
+local specWarnTraitor         = mod:NewSpecialWarningStack(307814, nil, 2, nil, nil, 3, 6)
+local specWarnReturnInterrupt = mod:NewSpecialWarningInterrupt(307829, "HasInterrupt", nil, 2, 1, 2)
+local specWarnPechati         = mod:NewSpecialWarningCast(307814, nil, nil, nil, 1, 2) --предатель
+local specWarnFlame           = mod:NewSpecialWarningMoveAway(307839, nil, nil, nil, 3, 2)
+local specWarnSveaz           = mod:NewSpecialWarningYou(308516, nil, nil, nil, 3, 2)
+local yellFlame               = mod:NewYell(307839, nil, nil, nil, "YELL") --Огонь
+local yellFlameFade           = mod:NewShortFadesYell(307839, nil, nil, nil, "YELL")
+local yellCastsvFade          = mod:NewShortFadesYell(308520)
 
-local timerInternalbleeding	   			= mod:NewCDTimer(28, 307833)
-local timerSveazi						= mod:NewCDTimer(28, 308517, nil, nil, nil, 2)
-local timerPriziv						= mod:NewCDTimer(120, 307852, nil, nil, nil, 4)
-local timerkik							= mod:NewCDTimer(15, 307829, nil, nil, nil, 3)
-local timerShkval						= mod:NewCDTimer(20, 307821, nil, nil, nil, 3)
-local timerCowardice	       			= mod:NewCDTimer(33, 307834)
-local timerFlame		       			= mod:NewCDTimer(15, 307839)
-local timerBreathNightmare	    		= mod:NewCDTimer(15, 308512)
-local timerAmonstrousblow	   			= mod:NewCDTimer(15, 307845)
+local timerInternalbleeding = mod:NewCDTimer(28, 307833)
+local timerSveazi           = mod:NewCDTimer(28, 308517, nil, nil, nil, 2)
+local timerPriziv           = mod:NewCDTimer(120, 307852, nil, nil, nil, 4)
+local timerkik              = mod:NewCDTimer(15, 307829, nil, nil, nil, 3)
+local timerShkval           = mod:NewCDTimer(20, 307821, nil, nil, nil, 3)
+local timerCowardice        = mod:NewCDTimer(33, 307834)
+local timerFlame            = mod:NewCDTimer(15, 307839)
+local timerBreathNightmare  = mod:NewCDTimer(15, 308512)
+local timerAmonstrousblow   = mod:NewCDTimer(15, 307845)
 
-mod:AddSetIconOption("SetIconOnSveazTarget", 314606, true, true, {5, 6, 7})
-mod:AddSetIconOption("SetIconOnFlameTarget", 307839, true, true, {1, 2, 3})
+mod:AddSetIconOption("SetIconOnSveazTarget", 314606, true, true, { 5, 6, 7 })
+mod:AddSetIconOption("SetIconOnFlameTarget", 307839, true, true, { 1, 2, 3 })
 mod:AddBoolOption("AnnounceSveaz", false)
 mod:AddBoolOption("AnnounceFlame", false)
 mod:AddBoolOption("AnnounceKnopk", false)
+mod:AddBoolOption("AnnounceOFF", false)
 mod:AddBoolOption("RangeFrame", true)
 
 local SveazTargets = {}
@@ -105,7 +106,7 @@ function mod:SPELL_CAST_START(args)
 		specWarnshkval:Show()
 	elseif args:IsSpellID(308520) then
 		yellCastsvFade:Countdown(308520)
-	elseif  args:IsSpellID(307852) and self:AntiSpam(2) then
+	elseif args:IsSpellID(307852) and self:AntiSpam(2) then
 		warnPriziv:Show()
 		timerPriziv:Start()
 	elseif args:IsSpellID(308512) then
@@ -126,12 +127,11 @@ function mod:SPELL_CAST_SUCCESS(args)
 	end
 end
 
-
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(307815) then
 		if args:IsPlayer() and (args.amount or 1) >= 2 then
-				specWarnTraitor:Show(args.amount)
-				specWarnTraitor:Play("stackhigh")
+			specWarnTraitor:Show(args.amount)
+			specWarnTraitor:Play("stackhigh")
 		end
 	elseif args:IsSpellID(307839) then
 		FlameTargets[#FlameTargets + 1] = args.destName
@@ -142,7 +142,7 @@ function mod:SPELL_AURA_APPLIED(args)
 			yellFlameFade:Countdown(307839)
 			if self.Options.RangeFrame then
 				DBM.RangeCheck:Show(12)
-		 	end
+			end
 		end
 	elseif args:IsSpellID(308517, 308620) then
 		SveazTargets[#SveazTargets + 1] = args.destName
@@ -155,11 +155,11 @@ function mod:SPELL_AURA_APPLIED(args)
 		if args:IsPlayer() then
 			specCowardice:Show()
 			if self.Options.RangeFrame then
-           		DBM.RangeCheck:Show(6)
-        	end
+				DBM.RangeCheck:Show(6)
+			end
 		end
 	elseif args:IsSpellID(307833) then
-        timerInternalbleeding:Start()
+		timerInternalbleeding:Start()
 		warnInternalbleeding:Show(args.destName, args.amount or 1)
 	end
 end
@@ -174,7 +174,7 @@ function mod:SPELL_AURA_REMOVED(args)
 		end
 		if self.Options.RangeFrame then
 			DBM.RangeCheck:Hide()
-	 end
+		end
 	elseif args:IsSpellID(308517, 308620) then
 		if self.Options.SetIconOnSveazTarget then
 			self:SetIcon(args.destName, 0)
@@ -183,15 +183,15 @@ function mod:SPELL_AURA_REMOVED(args)
 end
 
 function mod:CHAT_MSG_SAY(msg)
-	if strmatch(msg, L.Razr) then
+	if strmatch(msg, L.Razr) and mod.Options.AnnounceOFF then
 		specWarnRazrsveaz:Show()
 	end
 end
 
 function mod:SPELL_INTERRUPT(args)
-	if  args:IsSpellID(307829) then
-	timerkik:Start()
-	specWarnReturnInterrupt:Show()
+	if args:IsSpellID(307829) then
+		timerkik:Start()
+		specWarnReturnInterrupt:Show()
 	end
 end
 
@@ -199,6 +199,7 @@ do
 	local function sort_by_group(v1, v2)
 		return DBM:GetRaidSubgroup(UnitName(v1)) < DBM:GetRaidSubgroup(UnitName(v2))
 	end
+
 	function mod:SetFlameIcons()
 		table.sort(FlameTargets, sort_by_group)
 		for _, v in ipairs(FlameTargets) do
@@ -214,9 +215,10 @@ do
 			end
 			FlameIcons = FlameIcons - 1
 		end
-			warnFlame:Show(table.concat(FlameTargets, "<, >"))
-			table.wipe(FlameTargets)
+		warnFlame:Show(table.concat(FlameTargets, "<, >"))
+		table.wipe(FlameTargets)
 	end
+
 	function mod:SetSveazIcons()
 		if DBM:GetRaidRank() >= 0 then
 			table.sort(SveazTargets, sort_by_group)
@@ -281,5 +283,5 @@ function mod:UNIT_DIED(args)
 		timerInternalbleeding:Start(64)
 		warnInternalbgPre:Schedule(59)
 		timerShkval:Start(60)
- end
+	end
 end
--- DBM-WorldBoss/localization.ru.lua
+++ DBM-WorldBoss/localization.ru.lua
@@ -5,48 +5,49 @@ local L
 -- Норигорн
 L = DBM:GetModLocalization("Norigorn")
 
-L:SetGeneralLocalization{
+L:SetGeneralLocalization {
 	name = "Норигорн"
 }
 
-L:SetTimerLocalization{
+L:SetTimerLocalization {
 }
 
-L:SetWarningLocalization{
+L:SetWarningLocalization {
 }
 
-L:SetMiscLocalization{
+L:SetMiscLocalization {
 }
-L:SetOptionLocalization{
+L:SetOptionLocalization {
 }
 
 L = DBM:GetModLocalization("Zort")
 
-L:SetGeneralLocalization{
+L:SetGeneralLocalization {
 	name = "Зорт"
 }
 
-L:SetTimerLocalization{
+L:SetTimerLocalization {
 }
 
-L:SetWarningLocalization{
-	KnopSv				= "Нажмите кнопку (Разорвать связь!)"
+L:SetWarningLocalization {
+	KnopSv = "Нажмите кнопку (Разорвать связь!)"
 }
 
-L:SetMiscLocalization{
-	Pechat				= "Печать: Предательство {rt%d} установлена на %s",
-	Flame				= "Пламя Кошмаров {rt%d} установлена на %s",
-	Razr				= "Разрываю цепь",
-	Sveaz				= "Кошмарная цепь {rt%d} установлена на %s",
-	Lic  				= "Лик Зорта",
-	Shup	 			= "Щупальце-плеть Зорта",
-	Cudo 				= "Чудовищное щупальце Зорта"
+L:SetMiscLocalization {
+	Pechat = "Печать: Предательство {rt%d} установлена на %s",
+	Flame  = "Пламя Кошмаров {rt%d} установлена на %s",
+	Razr   = "Разрываю цепь",
+	Sveaz  = "Кошмарная цепь {rt%d} установлена на %s",
+	Lic    = "Лик Зорта",
+	Shup   = "Щупальце-плеть Зорта",
+	Cudo   = "Чудовищное щупальце Зорта"
 }
-L:SetOptionLocalization{
-	SetIconOnSveazTarget	= "Устанавливать метки на цели заклинания $spell:314606",
-	SetIconOnFlameTarget	= "Устанавливать метки на цели заклинания $spell:307839",
-	AnnounceFlame			= "Объявлять игроков, на кого установлена метка $spell:307839, в рейд чат",
-	RangeFrame				= "Показывать окно проверки дистанции(6/12/20)",
-	AnnounceKnopk			= "Сказать в белый чат об нажитии кнопки",
-	AnnounceSveaz			= "Объявить игрока на которого установлена метка $spell:314606, в рейд чат"
+L:SetOptionLocalization {
+	SetIconOnSveazTarget = "Устанавливать метки на цели заклинания $spell:314606",
+	SetIconOnFlameTarget = "Устанавливать метки на цели заклинания $spell:307839",
+	AnnounceFlame        = "Объявлять игроков, на кого установлена метка $spell:307839, в рейд чат",
+	RangeFrame           = "Показывать окно проверки дистанции(6/12/20)",
+	AnnounceKnopk        = "Сказать в белый чат об нажитии кнопки",
+	AnnounceOFF          = "Выключить анонс нажатия кнопки",
+	AnnounceSveaz        = "Объявить игрока на которого установлена метка $spell:314606, в рейд чат"
 }
